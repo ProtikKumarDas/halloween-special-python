a2ede04ec37d23e2a8b20ce8ba93a1ba06375177
_PROTIK <69142530+ProtikKumarDas@users.noreply.github.com>
2020-11-02 11:10:17 +0600

    updated

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+from time import sleep
+pygame.init()
+window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load('first.mp3')
+pygame.mixer.music.play()
+sleep(5)
+pygame.mixer.music.load('second.mp3')
+pygame.mixer.music.play()
+sleep(1)
+image = pygame.image.load('image.jpg')
+window.blit(image,(0,0))
+pygame.display.update()
+sleep(3)
